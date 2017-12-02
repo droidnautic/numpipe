407fbc984bf66b3c9b4a614f95486622f579632c
droidnautic <rrolsto1@binghamton.edu>
2017-12-01 20:29:28 -0500

    clean up

--- .numpipe1.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/csvb/numpipe/numpipe1.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/csvb/numpipe/numpipe1.ko /home/csvb/numpipe/numpipe1.o /home/csvb/numpipe/numpipe1.mod.o
